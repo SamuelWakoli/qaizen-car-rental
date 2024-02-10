1535b7c3e6fff5844c033162129c4538d784bc74
sam <swwakoli@gmail.com>
2024-02-10 11:29:48 +0300

    Asserted Google Auth works well

--- app/src/main/java/com/qaizen/car_rental_qaizen/MainActivity.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/MainActivity.kt
@@ -2,46 +2,69 @@ package com.qaizen.car_rental_qaizen
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.firebase.auth.FirebaseAuth
+import com.qaizen.car_rental_qaizen.ui.presentation.navigation.NavGraph
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.AuthViewModel
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.sign_in_with_google.GoogleAuthUiClient
 import com.qaizen.car_rental_qaizen.ui.theme.QaizenTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val googleAuthUiClient by lazy {
+        GoogleAuthUiClient(
+            context = this,
+            oneTapClient = Identity.getSignInClient(this)
+        )
+    }
+    private val currentUser = FirebaseAuth.getInstance().currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+
             QaizenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val authViewModel = hiltViewModel<AuthViewModel>()
+                // handles Google Sign in
+                val launcher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult(),
+                    onResult = {
+                        if (it.resultCode == RESULT_OK) {
+                            lifecycleScope.launch {
+                                val signInResult = googleAuthUiClient.signInByIntent(
+                                    intent = it.data ?: return@launch
+                                )
+                                authViewModel.onSignInResult(signInResult)
+                            }
+                        }
+                    }
+                )
+                NavGraph(
+                    authViewModel = authViewModel,
+                    onSignInWithGoogle = {
+                        // now Sign in with Google
+                        lifecycleScope.launch {
+                            val signInIntentSender = googleAuthUiClient.signIn()
+                            launcher.launch(
+                                IntentSenderRequest.Builder(
+                                    signInIntentSender ?: return@launch
+                                ).build()
+                            )
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QaizenTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/navigation/NavGraph.kt
@@ -0,0 +1,53 @@
+package com.qaizen.car_rental_qaizen.ui.presentation.navigation
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseAuth
+import com.qaizen.car_rental_qaizen.ui.presentation.composables.GoogleSignInButton
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.AuthViewModel
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+    authViewModel: AuthViewModel,
+    onSignInWithGoogle: () -> Unit,
+) {
+    val uiState = authViewModel.uiState.collectAsState().value
+    val context = LocalContext.current
+
+    Scaffold {innerPadding ->
+        Column(
+            Modifier.fillMaxSize().padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Current user: ${FirebaseAuth.getInstance().currentUser?.displayName}")
+            Spacer(modifier = Modifier.size(26.dp))
+            GoogleSignInButton(modifier = Modifier.widthIn(max = 300.dp), onClick = onSignInWithGoogle)
+            Spacer(modifier = Modifier.size(10.dp))
+            Text(text = "Auth Success: ${uiState.isSignInSuccess}")
+            Spacer(modifier = Modifier.size(10.dp))
+            Button(onClick = { FirebaseAuth.getInstance().signOut().also{
+                Toast.makeText(context, "Signed Out", Toast.LENGTH_SHORT).show()
+            } }) {
+                Text(text = "Sign Out")
+            }
+        }
+    }
+}
