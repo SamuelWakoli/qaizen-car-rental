11b431f2eaded5614e1f4fd17146c84b9c0b9816
sam <swwakoli@gmail.com>
2024-02-11 02:28:23 +0300

    Setup for the NavGraph

--- app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/navigation/NavGraph.kt
@@ -1,29 +1,19 @@
 package com.qaizen.car_rental_qaizen.ui.presentation.navigation
 
-import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import com.google.firebase.auth.FirebaseAuth
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.firebase.auth.FirebaseUser
-import com.qaizen.car_rental_qaizen.ui.presentation.composables.GoogleSignInButton
 import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.AuthViewModel
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.ForgotPasswordScreen
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.RegisterScreen
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.auth.SignInScreen
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.contact_us.ContactUsScreen
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.home.HomeScreen
 
 
 val NavHostController.canUserNavigateUp: Boolean
@@ -36,28 +26,36 @@ fun NavGraph(
     authViewModel: AuthViewModel,
     onSignInWithGoogle: () -> Unit,
 ) {
-    val uiState = authViewModel.uiState.collectAsState().value
-    val context = LocalContext.current
+    val navHostController = rememberNavController()
 
-    Scaffold {innerPadding ->
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Current user: ${FirebaseAuth.getInstance().currentUser?.displayName}")
-            Spacer(modifier = Modifier.size(26.dp))
-            GoogleSignInButton(modifier = Modifier.widthIn(max = 300.dp), onClick = onSignInWithGoogle)
-            Spacer(modifier = Modifier.size(10.dp))
-            Text(text = "Auth Success: ${uiState.isSignInSuccess}")
-            Spacer(modifier = Modifier.size(10.dp))
-            Button(onClick = { FirebaseAuth.getInstance().signOut().also{
-                Toast.makeText(context, "Signed Out", Toast.LENGTH_SHORT).show()
-            } }) {
-                Text(text = "Sign Out")
-            }
+
+    NavHost(
+        navController = navHostController,
+        startDestination = if (currentUser == null) Screens.SignInScreen.route
+        else Screens.HomeScreen.route
+    ) {
+        composable(Screens.SignInScreen.route) {
+            SignInScreen(
+                authViewModel = authViewModel,
+                navHostController = navHostController,
+                onSignInWithGoogle = onSignInWithGoogle,
+            )
+        }
+        composable(Screens.RegisterScreen.route) {
+            RegisterScreen(
+                authViewModel = authViewModel,
+                navHostController = navHostController,
+                onSignInWithGoogle = onSignInWithGoogle,
+            )
+        }
+        composable(Screens.ForgotPasswordScreen.route) {
+            ForgotPasswordScreen(navHostController = navHostController, viewModel = authViewModel)
+        }
+        composable(Screens.HomeScreen.route) {
+            HomeScreen()
+        }
+        composable(Screens.ContactUsScreen.route) {
+            ContactUsScreen()
         }
     }
 }
