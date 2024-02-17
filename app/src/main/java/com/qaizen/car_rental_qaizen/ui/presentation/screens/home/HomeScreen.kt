1e999ee147d26ae76f706adccfdbfeb72689255e
sam <swwakoli@gmail.com>
2024-02-17 11:31:47 +0300

    Added the app bar and bottom nav bar to HomeScreen

--- app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/home/HomeScreen.kt
@@ -2,37 +2,45 @@ package com.qaizen.car_rental_qaizen.ui.presentation.screens.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.qaizen.car_rental_qaizen.R
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.bottom_nav_pages.favorites.FavoritesPage
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.bottom_nav_pages.home.HomePage
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.bottom_nav_pages.more.MorePage
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.bottom_nav_pages.service.ServicePage
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.home.bottom_nav.HomeBottomNavBar
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.home.bottom_nav.bottomNavItems
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.home.top_app_bar.HomeTopAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen() {
 
-    val context = LocalContext.current
     val topAppBarScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    val currentUser = Firebase.auth.currentUser
+
+    val snackbarHostState = remember { SnackbarHostState() }
+    val bottomNavHostController = rememberNavController()
+
+    // TODO: Add a firestore listener to check for new version of the app.
+    //  Set the initial version to 1.0.0
+    //  Do this check inside the view model
+
 
     Box {
         Image(
@@ -43,29 +51,42 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         )
 
         Scaffold(
+            snackbarHost = { SnackbarHost(snackbarHostState) },
             topBar = {
-                CenterAlignedTopAppBar(
-                    scrollBehavior = topAppBarScrollBehavior,
-                    title = { Text(text = "Home", color = MaterialTheme.colorScheme.primary) })
+                HomeTopAppBar(
+                    topAppBarScrollBehavior = topAppBarScrollBehavior,
+                    bottomNavHostController = bottomNavHostController,
+                )
+            },
+            bottomBar = {
+                HomeBottomNavBar(
+                    bottomNavHostController = bottomNavHostController,
+                    bottomNavItems = bottomNavItems
+                )
             },
             containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.85f),
         ) { innerPadding ->
-            Column(
-                modifier = modifier
+
+            NavHost(
+                navController = bottomNavHostController,
+                startDestination = bottomNavItems.first().route,
+                modifier = Modifier
+                    .fillMaxSize()
                     .padding(innerPadding)
                     .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
             ) {
-
-
-                AsyncImage(
-                    model = currentUser?.photoUrl,
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .size(100.dp)
-                        .clip(CircleShape)
-                )
-                Text(text = "Current User: ${currentUser?.displayName}")
+                composable(bottomNavItems.first().route) {
+                    HomePage()
+                }
+                composable(bottomNavItems[1].route) {
+                    ServicePage()
+                }
+                composable(bottomNavItems[2].route) {
+                    FavoritesPage()
+                }
+                composable(bottomNavItems[3].route) {
+                    MorePage()
+                }
             }
         }
     }
