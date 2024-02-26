f06b14dc2f1623a7930f15af2df07b01c13a009c
sam <swwakoli@gmail.com>
2024-02-26 18:28:30 +0300

    Created PaymentInfoScreen

--- app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/payment_info/PaymentInfoScreen.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/payment_info/PaymentInfoScreen.kt
@@ -1,10 +1,144 @@
 package com.qaizen.car_rental_qaizen.ui.presentation.screens.profile_section.payment_info
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.qaizen.car_rental_qaizen.R
+import com.qaizen.car_rental_qaizen.ui.presentation.navigation.canUserNavigateUp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PaymentInfoScreen(windowSize: WindowSizeClass, navHostController: NavHostController) {
 
+    var phone by remember {
+        mutableStateOf("0700000000")
+    }
+    var phoneError by remember {
+        mutableStateOf(false)
+    }
+
+    val context = LocalContext.current
+
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            navigationIcon = {
+                IconButton(onClick = { if (navHostController.canUserNavigateUp) navHostController.navigateUp() }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Navigate back",
+                    )
+                }
+            },
+            title = { Text(text = "Payment Information") },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+        )
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .widthIn(max = 500.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable._200px_m_pesa_logo_01_svg),
+                    contentDescription = null,
+                    modifier = Modifier.width(200.dp)
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Our app now accepts payments via Mpesa, a safe and convenient mobile " +
+                            "money service. Enjoy instant payments and the ease of paying directly from " +
+                            "your phone. Mpesa is the only payment option available at the moment."
+                )
+                Spacer(modifier = Modifier.height(64.dp))
+                OutlinedTextField(
+                    value = phone,
+                    onValueChange = { value ->
+                        phone = value
+                        phoneError = value.isEmpty()
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.large,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Phone,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text(text = "Mpesa Phone Number")
+                    },
+                    supportingText = if (phoneError) {
+                        { Text(text = "Phone Number is required") }
+                    } else null,
+                    isError = phoneError,
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Words,
+                        autoCorrect = false,
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done,
+                    ),
+                )
+
+
+                Spacer(modifier = Modifier.height(32.dp))
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Text(text = "Save", style = MaterialTheme.typography.titleMedium)
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(imageVector = Icons.Outlined.CheckCircle, contentDescription = null)
+                }
+            }
+        }
+    }
 }
