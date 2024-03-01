a45ee0b530df11e241e29fd0c40fd67a95e5f496
sam <swwakoli@gmail.com>
2024-03-01 23:21:50 +0300

    BookingScreen TimePickerDialog design

---
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/booking/booking_screen/BookingTimePickerDialog.kt
@@ -0,0 +1,42 @@
+package com.qaizen.car_rental_qaizen.ui.presentation.screens.booking.booking_screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookingTimePickerDialog(onDismissRequest: () -> Unit, timePickerState: TimePickerState) {
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                TimePicker(state = timePickerState)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = onDismissRequest) {
+                        Text("Okay")
+                    }
+                }
+            }
+        }
+    }
+}
