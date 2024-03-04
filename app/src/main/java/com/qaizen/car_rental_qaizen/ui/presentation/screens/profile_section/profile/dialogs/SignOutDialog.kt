1d6979f92da3a6e19c8a94b2fa0ceb3d030b5b20
sam <swwakoli@gmail.com>
2024-03-04 15:49:49 +0300

    Created SignOutDialog

---
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/profile/dialogs/SignOutDialog.kt
@@ -0,0 +1,96 @@
+package com.qaizen.car_rental_qaizen.ui.presentation.screens.profile_section.profile.dialogs
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Logout
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignOutDialog(
+    onConfirmation: () -> Unit,
+    onDismissRequest: () -> Unit,
+) {
+    BasicAlertDialog(onDismissRequest = { onDismissRequest() }) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer,
+            )
+        ) {
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Logout,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .size(32.dp)
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(
+                    text = "Sign out",
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(text = "You are about to sign out from the app. Your personal information and app data will remain stored in the cloud.")
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    TextButton(onClick = {
+                        onDismissRequest()
+                    }) {
+                        Text(text = "Cancel")
+                    }
+                    Spacer(modifier = Modifier.size(8.dp))
+                    TextButton(onClick = {
+                        onConfirmation()
+// TODO: handle error when sign out fails
+                        onDismissRequest()
+                    }) {
+                        Text(text = "Okay")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun SignOutDialogPreview() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.surface)
+    ) {
+        SignOutDialog(
+            onConfirmation = {},
+            onDismissRequest = {}
+        )
+    }
+}
