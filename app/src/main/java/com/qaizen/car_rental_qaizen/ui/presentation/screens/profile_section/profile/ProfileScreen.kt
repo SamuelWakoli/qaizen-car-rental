81f164870c8998a33d27f9bd6c8d4fff523d8904
sam <swwakoli@gmail.com>
2024-03-04 15:49:57 +0300

    Created DeleteProfileDialog

--- app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/profile/ProfileScreen.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/profile/ProfileScreen.kt
@@ -15,9 +15,17 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import com.qaizen.car_rental_qaizen.ui.presentation.navigation.Screens
 import com.qaizen.car_rental_qaizen.ui.presentation.navigation.canUserNavigateUp
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.profile_section.profile.dialogs.DeleteProfileDialog
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.profile_section.profile.dialogs.SignOutDialog
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,57 +34,100 @@ fun ProfileScreen(
     navHostController: NavHostController,
 ) {
 
+    var showSignOutDialog by remember { mutableStateOf(false) }
+    var showDeleteProfileDialog by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
 
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                navigationIcon = {
-                    IconButton(onClick = { if (navHostController.canUserNavigateUp) navHostController.navigateUp() }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = "Navigate back"
-                        )
-                    }
-                },
-                title = { Text(text = "Profile") },
-                actions = {
-                    IconButton(onClick = {
-                        navHostController.navigate(Screens.EditProfileScreen.route) {
-                            launchSingleTop = true
-                        }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Outlined.Edit,
-                            contentDescription = "Edit Profile",
-                            tint = MaterialTheme.colorScheme.tertiary
-                        )
-                    }
-                },
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    titleContentColor = MaterialTheme.colorScheme.primary,
+
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(navigationIcon = {
+            IconButton(onClick = { if (navHostController.canUserNavigateUp) navHostController.navigateUp() }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Navigate back"
                 )
-            )
-        }
-    ) { innerPadding: PaddingValues ->
+            }
+        }, title = { Text(text = "Profile") }, actions = {
+            IconButton(onClick = {
+                navHostController.navigate(Screens.EditProfileScreen.route) {
+                    launchSingleTop = true
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Outlined.Edit,
+                    contentDescription = "Edit Profile",
+                    tint = MaterialTheme.colorScheme.tertiary
+                )
+            }
+        }, colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        )
+        )
+    }) { innerPadding: PaddingValues ->
         when (windowSize.widthSizeClass) {
             WindowWidthSizeClass.Expanded, WindowWidthSizeClass.Medium -> {
-                ProfileScreenExpanded(
-                    innerPadding = innerPadding,
+                ProfileScreenExpanded(innerPadding = innerPadding,
                     navHostController = navHostController,
-                    onClickSignOut = {},
-                    onClickDeleteAccount = {}
-                )
+                    onClickSignOut = { showSignOutDialog = true },
+                    onClickDeleteAccount = { showDeleteProfileDialog = true })
             }
 
             else -> {
-                ProfileScreenCompact(
-                    innerPadding = innerPadding,
+                ProfileScreenCompact(innerPadding = innerPadding,
                     navHostController = navHostController,
-                    onClickSignOut = {},
-                    onClickDeleteAccount = {}
-                )
+                    onClickSignOut = { showSignOutDialog = true },
+                    onClickDeleteAccount = { showDeleteProfileDialog = true })
             }
         }
 
+        if (showSignOutDialog) SignOutDialog(onConfirmation = {
+            coroutineScope.launch {
+//                    profileScreenViewModel.signOut(
+//                        onSuccess = {
+//                            navController.navigate(route = Screens.SignInScreen.route) {
+//                                popUpTo(Screens.HomeScreen.route) {
+//                                    inclusive = true
+//                                }
+//                            }
+//                        },
+//                        onFailure = {
+//                            profileScreenViewModel.hideOrShowSignOutDialog()
+//                            Toast.makeText(
+//                                context,
+//                                "Failed to sign out, Please try again later",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//                    )
+            }
+        }, onDismissRequest = {
+            showSignOutDialog = false
+//                profileScreenViewModel.hideOrShowSignOutDialog()
+        })
+
+        if (showDeleteProfileDialog) DeleteProfileDialog(onConfirmation = {
+            coroutineScope.launch {
+//                    profileScreenViewModel.deleteProfileAndData(
+//                        onSuccess = {
+//                            navController.navigate(route = Screens.SignInScreen.route) {
+//                                popUpTo(Screens.HomeScreen.route) {
+//                                    inclusive = true
+//                                }
+//                            }
+//                        },
+//                        onFailure = { exception: Exception ->
+//                            profileScreenViewModel.hideOrShowDeleteProfileDialog()
+//                            Toast.makeText(
+//                                context,
+//                                "Failed to delete profile and data: ${exception.message}",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                        }
+//                    )
+            }
+        }, onDismissRequest = {
+            showDeleteProfileDialog = false
+//                profileScreenViewModel.hideOrShowDeleteProfileDialog()
+        })
     }
 }
