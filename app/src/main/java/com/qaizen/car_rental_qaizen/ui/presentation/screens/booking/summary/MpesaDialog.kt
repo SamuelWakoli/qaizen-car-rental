5ff77e5d9234924d038192ce113dff0f386183e9
sam <swwakoli@gmail.com>
2024-03-04 12:23:01 +0300

    Created MpesaDialog

---
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/booking/summary/MpesaDialog.kt
@@ -0,0 +1,72 @@
+package com.qaizen.car_rental_qaizen.ui.presentation.screens.booking.summary
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.qaizen.car_rental_qaizen.R
+
+@Composable
+fun MpesaDialog(onDismissRequest: () -> Unit, onClickPay: () -> Unit) {
+    var phone by remember { mutableStateOf("0712345678") }
+    var phoneError by remember { mutableStateOf(false) }
+
+
+    Dialog(onDismissRequest = onDismissRequest) {
+        Card {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable._200px_m_pesa_logo_01_svg),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .height(100.dp),
+                    contentScale = ContentScale.FillHeight
+                )
+                Text(text = "Please confirm your Mpesa phone number")
+                TextField(
+                    value = phone,
+                    onValueChange = { value ->
+                        phone = value
+                        phoneError = value.isEmpty()
+                    },
+                    label = { Text("Phone") },
+                    placeholder = { Text("Enter number of days") },
+                    isError = phoneError,
+                    shape = MaterialTheme.shapes.medium,
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done,
+                    ),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                ElevatedButton(onClick = onClickPay) {
+                    Text(text = "Initiate Payment", style = MaterialTheme.typography.bodyLarge)
+                }
+            }
+        }
+    }
+}
