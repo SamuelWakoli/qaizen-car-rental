8c44675314a1d18643d2f00f7cfd326b65fda257
sam <swwakoli@gmail.com>
2024-03-24 20:46:14 +0300

    design for the add vehicle screen

--- admin/src/main/java/com/qaizen/admin/vehicles/presentation/add_vehicle/AddVehicleScreen.kt
+++ admin/src/main/java/com/qaizen/admin/vehicles/presentation/add_vehicle/AddVehicleScreen.kt
@@ -1,18 +1,33 @@
 package com.qaizen.admin.vehicles.presentation.add_vehicle
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.AddAPhoto
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.qaizen.admin.navigation.canUserNavigateUp
 
@@ -25,21 +40,83 @@ fun AddVehicleScreen(
 ) {
 
     Scaffold(topBar = {
-        CenterAlignedTopAppBar(navigationIcon = {
-            IconButton(onClick = {
-                if (navHostController.canUserNavigateUp) navHostController.navigateUp()
-            }) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "Navigate back"
-                )
-            }
-        }, title = { Text(text = "Add Vehicle") })
+        CenterAlignedTopAppBar(
+            navigationIcon = {
+                IconButton(onClick = {
+                    if (navHostController.canUserNavigateUp) navHostController.navigateUp()
+                }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Navigate back"
+                    )
+                }
+            },
+            title = { Text(text = "Add Vehicle") },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                titleContentColor = MaterialTheme.colorScheme.primary,
+                navigationIconContentColor = MaterialTheme.colorScheme.secondary,
+            ),
+        )
     }) { innerPadding ->
         Column(
-            modifier = modifier.padding(innerPadding)
+            modifier = modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-
+            Column(
+                modifier = Modifier
+                    .widthIn(max = 600.dp)
+                    .padding(16.dp),
+            ) {
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { value -> },
+                    label = { Text(text = "Name") },
+                    shape = MaterialTheme.shapes.medium,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { value -> },
+                    label = { Text(text = "Price per day") },
+                    shape = MaterialTheme.shapes.medium,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { value -> },
+                    label = { Text(text = "Number Plate") },
+                    shape = MaterialTheme.shapes.medium,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { value -> },
+                    label = { Text(text = "Type") },
+                    shape = MaterialTheme.shapes.medium,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedTextField(
+                    value = "",
+                    onValueChange = { value -> },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = { Text(text = "Description") },
+                    shape = MaterialTheme.shapes.medium,
+                    minLines = 3,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                OutlinedButton(onClick = { /*TODO*/ }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(imageVector = Icons.Outlined.AddAPhoto, contentDescription = null)
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(text = "Add Images", style = MaterialTheme.typography.bodyLarge)
+                    }
+                }
+            }
         }
     }
 }
