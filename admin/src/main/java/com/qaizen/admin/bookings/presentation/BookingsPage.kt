3baa12c23b39576f823424fc970ab73d953b4676
sam <swwakoli@gmail.com>
2024-03-31 16:28:12 +0300

    Implemented booking items

--- admin/src/main/java/com/qaizen/admin/bookings/presentation/BookingsPage.kt
+++ admin/src/main/java/com/qaizen/admin/bookings/presentation/BookingsPage.kt
@@ -1,12 +1,22 @@
 package com.qaizen.admin.bookings.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -15,6 +25,7 @@ import androidx.navigation.NavHostController
 fun BookingsPage(
     windowSize: WindowSizeClass,
     navHostController: NavHostController,
+    bookingsViewModel: BookingsViewModel,
 ) {
 
     val itemMaxWidth = when (windowSize.widthSizeClass) {
@@ -22,20 +33,48 @@ fun BookingsPage(
         else -> 300.dp
     }
 
+    val bookingsList = bookingsViewModel.bookings.collectAsState().value
+
     Column(
         modifier = Modifier
     ) {
-        LazyVerticalStaggeredGrid(
-            modifier = Modifier, columns = StaggeredGridCells.Adaptive(itemMaxWidth)
-        ) {
-            items(20) {
-                BookingListItem(
-                    modifier = Modifier.padding(
-                        horizontal = 8.dp,
-                        vertical = 4.dp
-                    )
-                )
+        if (bookingsList == null) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                CircularProgressIndicator()
             }
-        }
+        } else if (bookingsList.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "4😶4", style = MaterialTheme.typography.displayLarge)
+                Spacer(modifier = Modifier.size(8.dp))
+                Text(text = "No bookings found")
+            }
+        } else
+
+            LazyVerticalStaggeredGrid(
+                modifier = Modifier, columns = StaggeredGridCells.Adaptive(itemMaxWidth)
+            ) {
+                items(bookingsList) { bookingItem ->
+
+                    BookingListItem(
+                        modifier = Modifier.padding(
+                            horizontal = 8.dp,
+                            vertical = 4.dp
+                        ),
+                        bookingItem = bookingItem,
+                    )
+                }
+            }
     }
 }
