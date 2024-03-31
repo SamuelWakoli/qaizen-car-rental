83a1fc22bc0deed650c12eb85301d6be23715417
sam <swwakoli@gmail.com>
2024-03-31 12:50:29 +0300

    Implemented PaymentInfoScreen

--- app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/payment_info/PaymentInfoScreen.kt
+++ app/src/main/java/com/qaizen/car_rental_qaizen/ui/presentation/screens/profile_section/payment_info/PaymentInfoScreen.kt
@@ -1,5 +1,6 @@
 package com.qaizen.car_rental_qaizen.ui.presentation.screens.profile_section.payment_info
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
@@ -17,6 +19,7 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material.icons.outlined.Phone
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,10 +27,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -43,13 +51,22 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.qaizen.car_rental_qaizen.R
 import com.qaizen.car_rental_qaizen.ui.presentation.navigation.canUserNavigateUp
+import com.qaizen.car_rental_qaizen.ui.presentation.screens.ProfileViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PaymentInfoScreen(windowSize: WindowSizeClass, navHostController: NavHostController) {
+fun PaymentInfoScreen(
+    windowSize: WindowSizeClass,
+    navHostController: NavHostController,
+    profileViewModel: ProfileViewModel,
+) {
+
+    val userData = profileViewModel.userData.collectAsState().value
+    var isSaving by remember { mutableStateOf(false) }
+
 
     var phone by remember {
-        mutableStateOf("0700000000")
+        mutableStateOf(userData?.mpesaPhone ?: "")
     }
     var phoneError by remember {
         mutableStateOf(false)
@@ -57,22 +74,39 @@ fun PaymentInfoScreen(windowSize: WindowSizeClass, navHostController: NavHostCon
 
     val context = LocalContext.current
 
-    Scaffold(topBar = {
-        CenterAlignedTopAppBar(
-            navigationIcon = {
-                IconButton(onClick = { if (navHostController.canUserNavigateUp) navHostController.navigateUp() }) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Navigate back",
-                    )
-                }
-            },
-            title = { Text(text = "Payment Information") },
-            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ),
-        )
-    }) { innerPadding ->
+    val snackbarHostState = remember { SnackbarHostState() }
+    var errorMessage by remember {
+        mutableStateOf("")
+    }
+    LaunchedEffect(errorMessage) {
+        if (errorMessage.isNotEmpty()) {
+            snackbarHostState.showSnackbar(
+                message = errorMessage,
+                withDismissAction = true,
+                duration = SnackbarDuration.Indefinite
+            )
+            errorMessage = ""
+        }
+    }
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { if (navHostController.canUserNavigateUp) navHostController.navigateUp() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Navigate back",
+                        )
+                    }
+                },
+                title = { Text(text = "Payment Information") },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+            )
+        }) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -94,7 +128,7 @@ fun PaymentInfoScreen(windowSize: WindowSizeClass, navHostController: NavHostCon
                 )
                 Spacer(modifier = Modifier.height(32.dp))
                 Text(
-                    text = "Our app now accepts payments via Mpesa, a safe and convenient mobile " +
+                    text = "[In development]\nOur app now accepts payments via Mpesa, a safe and convenient mobile " +
                             "money service. Enjoy instant payments and the ease of paying directly from " +
                             "your phone. Mpesa is the only payment option available at the moment."
                 )
@@ -131,10 +165,27 @@ fun PaymentInfoScreen(windowSize: WindowSizeClass, navHostController: NavHostCon
 
 
                 Spacer(modifier = Modifier.height(32.dp))
-                OutlinedButton(onClick = { /*TODO*/ }) {
+                OutlinedButton(onClick = {
+                    profileViewModel.updateUserData(
+                        userData = userData?.copy(mpesaPhone = phone)!!,
+                        onSuccess = {
+                            isSaving = false
+                            navHostController.navigateUp()
+                            Toast.makeText(context, "Profile updated", Toast.LENGTH_LONG).show()
+                        },
+                        onError = {
+                            isSaving = false
+                            errorMessage = it.message.toString()
+                        })
+                }) {
                     Text(text = "Save", style = MaterialTheme.typography.titleMedium)
                     Spacer(modifier = Modifier.width(8.dp))
-                    Icon(imageVector = Icons.Outlined.CheckCircle, contentDescription = null)
+                    if (isSaving) CircularProgressIndicator(
+                        modifier = Modifier.size(24.dp),
+                        strokeWidth = 2.dp,
+                        color = MaterialTheme.colorScheme.primary,
+                        trackColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                    ) else Icon(imageVector = Icons.Outlined.CheckCircle, contentDescription = null)
                 }
             }
         }
